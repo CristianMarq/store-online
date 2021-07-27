16a8a1db3a389dabc1b7c5da8c7cd971d1392432
Christian Marquez <marquezcristian920@gmail.com>
2021-07-26 20:31:53 -0600

    Agregando funcionaldad para crear elemento html para el producto

--- src/classes/Producto.class.js
+++ src/classes/Producto.class.js
@@ -3,7 +3,7 @@ export class Producto {
   _nombre;
   _descripcion;
   _precio;
-  _disponible;
+  _agotado;
   _creado;
 
   constructor(nombre, descripcion, precio) {
@@ -11,8 +11,7 @@ export class Producto {
     this._nombre = nombre;
     this._descripcion = descripcion;
     this._precio = precio;
-    this._disponible = false;
+    this._agotado = true;
     this._creado = new Date();
   }
 }
-
---
+++ src/js/componentes.js
@@ -0,0 +1,29 @@
+// Referencias al dom
+
+const divProductos = document.querySelector("#list-products");
+
+const stringToHtml = (string) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(string, "text/html");
+  return doc.body.firstChild;
+};
+
+export const crearProductoHtml = (producto) => {
+  const elementHtml = stringToHtml(`
+  <li class="${producto._agotado ? "completed" : ""}" data-id="${producto._id}">
+    <div id="view">
+      <input class="toggle" type="checkbox" ${
+        producto._agotado ? "checked" : ""
+      } />
+      <label>${producto._nombre}</label>
+      <label>${producto._descripcion}</label>
+      <button id="destroy">x</button>
+      <br />
+      <label id="price">$${producto._precio}</label>
+    </div>
+  </li>`);
+
+  divProductos.append(elementHtml);
+
+  return elementHtml;
+};
